145593d1826909e9d82af82ed6cdea0e1bedfa27
jamieberrier <berrier.jamie@gmail.com>
2020-10-06 13:15:31 -0700

    adds reducers for users, loginForm, and currentUser

--- src/store.js
+++ src/store.js
@@ -1,10 +1,16 @@
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 
+import currentUser from './reducers/currentUser.js';
 import grid from './reducers/grid.js';
+import loginForm from './reducers/loginForm.js'
+import users from './reducers/users.js';
 
 const reducer = combineReducers({
-  grid
+  grid,
+  users,
+  loginForm,
+  currentUser
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
