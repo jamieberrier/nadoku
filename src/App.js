57da5b97dde93d079683d6f5346b2518d961ae03
jamieberrier <berrier.jamie@gmail.com>
2020-10-07 09:31:26 -0700

    connects to redux store, gets currentUser on mount, and adds Login & logout components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import './App.css';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import { getCurrentUser } from './actions/currentUser.js'
+
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.getCurrentUser()
+  }
 
   render() {
     return (
@@ -10,9 +19,19 @@ class App extends Component {
         <header className="App-header">
           <h1>Nadoku</h1>
         </header>
+        {this.props.currentUser 
+          ? <Logout />
+          : <Login />
+        }
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({ currentUser }) => {
+  return {
+    currentUser
+  }
+}
+
+export default connect(mapStateToProps, { getCurrentUser })(App);
