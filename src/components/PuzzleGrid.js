7654f6d83aa12751c8e2f94fce4f2b49069bca7f
jamieberrier <berrier.jamie@gmail.com>
2020-10-07 17:16:07 -0700

    creates puzzle grid component to display puzzle

---
+++ src/components/PuzzleGrid.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+import { setCellClass, updateCellValue } from '../actions/puzzle.js';
+
+const PuzzleGrid = ({ puzzle, updateCellValue }) => {
+
+  const handleOnChange = event => {
+    const { id, value } = event.target
+    const { rowindex } = event.target.dataset
+    updateCellValue(rowindex, id, value)
+  }
+  
+  const cells = puzzle.map((row, index) => 
+    <div className="row" key={index}>
+      {row.map(cell => {
+        const value = cell.value !== "." ? cell.value : ""
+        return <input
+          className={setCellClass(cell.coordinates)}
+          id={cell.coordinates}
+          key={cell.coordinates}
+          onChange={handleOnChange}
+          readOnly={cell.readOnly}
+          data-rowindex={index}
+          value={value} 
+        />
+      })}
+    </div>
+  )
+      
+  return (
+    <div className="grid">
+      {cells}
+    </div>
+  )
+}
+
+const mapStateToProps = ({ puzzle }) => {
+  return {
+    puzzle
+  }
+}
+
+export default connect(mapStateToProps, { updateCellValue })(PuzzleGrid);
