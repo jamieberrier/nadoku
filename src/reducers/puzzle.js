dcb88d69f5597bb0bf62cba17707cf1903408a8e
jamieberrier <berrier.jamie@gmail.com>
2020-10-07 17:17:24 -0700

    creates case for UPDATE_CELL_VALUE

--- src/reducers/puzzle.js
+++ src/reducers/puzzle.js
@@ -2,6 +2,20 @@ export default (state = [], action) => {
   switch (action.type) {
     case "SET_PUZZLE":
       return state.concat(action.puzzle)
+    case "UPDATE_CELL_VALUE":
+      const rowIndex = parseInt(action.rowindex)
+      const row = [...state[rowIndex]]
+      const newRow = row.map(cell => {
+        if (cell.coordinates === action.id) {
+          // { coordinates: cell.coordinates, value: action.value, readOnly: cell.readOnly }
+          return Object.assign({}, cell, {value: action.value})
+        }
+        // return all the ones not changing
+        return cell
+      })
+      
+      const newState = [...state.slice(0, rowIndex), newRow, ...state.slice(rowIndex+1)]
+      return newState
     default:
       return state;
   }
