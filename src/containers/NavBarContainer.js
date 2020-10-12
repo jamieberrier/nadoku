09e8a026f8868bc91b49a051893eebaa10a2fe48
jamieberrier <berrier.jamie@gmail.com>
2020-10-12 09:47:20 -0700

    updates header to only display app name

--- src/containers/NavBarContainer.js
+++ src/containers/NavBarContainer.js
@@ -1,24 +1,13 @@
 import React, { Component }  from 'react';
-import { connect } from 'react-redux';
-
-import Logout from '../components/Logout.js';
 
 class NavBarContainer extends Component {
   render() {
-    const { currentUser } = this.props
     return (
       <header className='App-header'>
-        <h1>Nadoku</h1>
-        {currentUser && <Logout />}
-    </header>
+        Nadoku
+      </header>
     )
   }
 }
 
-const mapStateToProps = ({ currentUser }) => {
-  return {
-    currentUser
-  }
-}
-
-export default connect(mapStateToProps, null)(NavBarContainer);
+export default NavBarContainer;
