42a74d6335affacfa459fc1ec2da3ad04f344a9f
jamieberrier <berrier.jamie@gmail.com>
2020-10-12 13:27:19 -0700

    changes to container component

--- src/containers/NumberContainer.js
+++ src/containers/NumberContainer.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import { Section } from 'react-bulma-components';
 
 import NumberPad from '../components/NumberPad';
+import { setSelectedNumber } from '../actions/selectedNumber.js';
 
 class NumberContainer extends Component {
+
+  handleOnClick = (event) => {
+    this.props.setSelectedNumber(event.target.value)
+  }
 
   render() {
     return (
       <Section>
-        <NumberPad />
+        <NumberPad handleOnClick={this.handleOnClick} />
       </Section>
     )
   }
 }
 
-export default NumberContainer;
+export default connect(null, ({ setSelectedNumber }))(NumberContainer);
