c36c2ef3907da323047d94f500fd5cb4002cc045
jamieberrier <berrier.jamie@gmail.com>
2020-10-13 13:36:28 -0700

    adds action creator for highlightCells and adds setCellClass

--- src/actions/puzzle.js
+++ src/actions/puzzle.js
@@ -15,9 +15,35 @@ export const updateCellValue = (rowindex, id, value) => {
   }
 }
 
+export const checkCellValue = cell => {
+  return {
+    type: 'CHECK_CELL_VALUE',
+    cell
+  }
+}
+
+export const highlightCells = value => {
+  return {
+    type: 'HIGHLIGHT_CELLS',
+    value
+  }
+}
+
 export const clearPuzzle = puzzle => {
   return {
     type: 'CLEAR_PUZZLE',
     puzzle
+  }
+}
+
+export const setCellClass = coordinates => {
+  if (coordinates === 'C3' || coordinates === 'F3' || coordinates === 'C6' || coordinates === 'F6') {
+    return "cell rightborder bottomborder"
+  } else if (coordinates.includes('C') || coordinates.includes('F')) {
+    return "cell bottomborder"
+  } else if (coordinates.includes('3') || coordinates.includes('6')) {
+    return "cell rightborder"
+  } else {
+    return "cell"
   }
 }
