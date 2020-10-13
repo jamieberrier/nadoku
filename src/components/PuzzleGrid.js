38aded11e3f59d7982476cb026e640b813a83020
jamieberrier <berrier.jamie@gmail.com>
2020-10-13 13:38:02 -0700

    moves setCellClass to actions/puzzle and adds disabled

--- src/components/PuzzleGrid.js
+++ src/components/PuzzleGrid.js
@@ -5,31 +5,19 @@ import { Container } from 'react-bulma-components';
 
 const PuzzleGrid = ({ puzzle, handleOnChange, handleOnClick }) => {
 
-  const setCellClass = coordinates => {
-    if (coordinates === 'C3' || coordinates === 'F3' || coordinates === 'C6' || coordinates === 'F6') {
-      return "cell rightborder bottomborder"
-    } else if (coordinates.includes('C') || coordinates.includes('F')) {
-      return "cell bottomborder"
-    } else if (coordinates.includes('3') || coordinates.includes('6')) {
-      return "cell rightborder"
-    } else {
-      return "cell"
-    }
-  }
-
   const cells = puzzle.map((row, index) => 
     <div className="row" key={index}>
       {row.map(cell => {
-        const value = cell.value !== "." ? cell.value : ""
         return <input
-          className={setCellClass(cell.coordinates)}
+          className={cell.className}
           id={cell.coordinates}
           key={cell.coordinates}
           onChange={handleOnChange}
           onClick={handleOnClick}
           readOnly={cell.readOnly}
           data-rowindex={index}
-          value={value} 
+          defaultValue={cell.value}
+          disabled={cell.disabled}
         />
       })}
     </div>
