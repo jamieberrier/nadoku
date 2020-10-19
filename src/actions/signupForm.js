840a5310b1965c90273cc6f6b66b34d71174941f
jamieberrier <berrier.jamie@gmail.com>
2020-10-19 16:11:14 -0700

    creates action creators for signupForm with UPDATE_SIGNUP_FORM, RESET_SIGNUP_FORM, and signu

---
+++ src/actions/signupForm.js
@@ -0,0 +1,49 @@
+import { setCurrentUser } from './currentUser.js';
+
+export const updateSignupForm = formData => {
+  return {
+      type: 'UPDATE_SIGNUP_FORM',
+      // use shorthand of just formData
+      formData
+  }
+}
+
+export const resetSignupForm = () => {
+  return {
+      type: 'RESET_SIGNUP_FORM'
+  }
+}
+
+// asyncronous action creators
+export const signup = ({email, password, passwordConfirmation}) => {
+  let bodyData = {
+    user: {
+      email,
+      password,
+      password_confirmation: passwordConfirmation
+    }
+  }
+
+  let configObj = {
+    credentials: 'include',
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(bodyData)
+  }
+
+  return dispatch => {
+    return fetch('http://localhost:3000/api/v1/users', configObj)
+    .then(response => response.json())
+    .then(user => {
+      if (user.errors) {
+        alert(user.errors)
+      } else {
+        dispatch(setCurrentUser(user.data.attributes))
+      }
+    })
+    .catch(error => alert(error.message))
+  }
+}
