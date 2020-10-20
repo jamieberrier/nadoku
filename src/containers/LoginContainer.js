9d19cc9faf314003838692798c0809a2e987e05f
jamieberrier <berrier.jamie@gmail.com>
2020-10-20 14:40:50 -0700

    changes to class component, utilizes bulma styling, and destructures mapStateToProps

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,55 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Section, Container, Form } from 'react-bulma-components';
 
 import { login } from '../actions/currentUser.js';
 import { updateLoginForm, resetLoginForm} from '../actions/loginForm.js';
+import ButtonFullWidth from '../components/ButtonFullWidth'
 
-const Login = ({ loginFormData, updateLoginForm, login, history, resetLoginForm }) => {
+const { Input, Field, Control, Label } = Form;
+class LoginContainer extends Component {
 
-  const handleOnChange = event => {
+  handleOnChange = event => {
     const { name, value } = event.target
     const updatedFormInfo = { 
-      ...loginFormData,
+      ...this.props.loginFormData,
       [name]: value
     }
-    updateLoginForm(updatedFormInfo)
+    this.props.updateLoginForm(updatedFormInfo)
   }
 
-  const handleSubmit = (event) => {
+  handleSubmit = event => {
     event.preventDefault()
-    login(loginFormData, history)
-    resetLoginForm()
+    this.props.login(this.props.loginFormData, this.props.history)
+    this.props.resetLoginForm()
   }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input 
-        name='email'
-        onChange={handleOnChange}
-        placeholder='Enter Email Address'
-        type='text'
-        value={loginFormData.email}
-      />
-      <input 
-        name='password'
-        onChange={handleOnChange}
-        placeholder='Enter Password'
-        type='text'
-        value={loginFormData.password}
-      />
-      <input 
-        type='submit'
-        value='Log In'
-      />
-    </form>
-  )
+  render() {
+    return (
+      <Section id='LoginSection'>
+        <Container id='LoginContainer'>
+          <Field>
+            <Label>Email</Label>
+            <Control>
+              <Input 
+                name='email'
+                onChange={this.handleOnChange}
+                placeholder='Enter Email Address'
+                type='text'
+                value={this.props.loginFormData.email}
+              />
+            </Control>
+          </Field>
+          <Field>
+            <Label>Password</Label>
+            <Control>
+              <Input 
+                name='password'
+                onChange={this.handleOnChange}
+                placeholder='Enter Password'
+                type='password'
+                value={this.props.loginFormData.password}
+              />
+            </Control>
+          </Field>
+          <Field>
+            <Control>
+              <ButtonFullWidth color={'dark'} text={'Log In'} handleOnClick={this.handleSubmit} />
+            </Control>
+          </Field>
+        </Container>
+      </Section>
+    )
+  }
 }
 
 // get state from store to use as props in this component
-const mapStateToProps = state => {
+const mapStateToProps = ({ loginForm }) => {
   return {
-    loginFormData: state.loginForm
+    loginFormData: loginForm
   }
 }
 
-export default connect(mapStateToProps, { updateLoginForm, login, resetLoginForm })(Login);
+export default connect(mapStateToProps, { updateLoginForm, login, resetLoginForm })(LoginContainer);
