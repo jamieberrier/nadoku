15064d973c55fe1914ae8c3219a1dc054ecb1141
jamieberrier <berrier.jamie@gmail.com>
2020-10-20 14:39:48 -0700

    deletes Section

--- src/containers/DifficultyContainer.js
+++ src/containers/DifficultyContainer.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Section, Container, Form } from 'react-bulma-components';
+import { Container, Form } from 'react-bulma-components';
 
 import { setDifficulty } from '../actions/difficulty';
 import { generateRawPuzzle } from '../actions/puzzleRaw.js';
@@ -34,23 +34,21 @@ class DifficultyContainer extends Component {
 
   render() {
     return (
-      <Section>
-        <Container id='DifficultyContainer'>
-          <Radio name={'difficulty'} value={'easy'} checked={this.state.selected === 'easy'} onChange={this.handleOnChange}> Easy</Radio>
-          <Radio name={'difficulty'} value={'medium'} checked={this.state.selected === 'medium'} onChange={this.handleOnChange}> Medium</Radio>
-          <Radio name={'difficulty'} value={'hard'} checked={this.state.selected === 'hard'} onChange={this.handleOnChange}> Hard</Radio>
-          <Radio name={'difficulty'} value={'very-hard'} checked={this.state.selected === 'very-hard'} onChange={this.handleOnChange}> Very Hard</Radio>
-          <Radio name={'difficulty'} value={'insane'} checked={this.state.selected === 'insane'} onChange={this.handleOnChange}> Insane</Radio>
-          <Radio name={'difficulty'} value={'inhuman'} checked={this.state.selected === 'inhuman'} onChange={this.handleOnChange}> Inhuman</Radio>
-          <Link to='/puzzle'>
-            <ButtonFullWidth 
-              color={'dark'} 
-              text={'Start Puzzle'} 
-              handleOnClick={this.handleOnClick} 
-            />
-          </Link>
-        </Container>
-      </Section>
+      <Container id='DifficultyContainer'>
+        <Radio name={'difficulty'} value={'easy'} checked={this.state.selected === 'easy'} onChange={this.handleOnChange}> Easy</Radio>
+        <Radio name={'difficulty'} value={'medium'} checked={this.state.selected === 'medium'} onChange={this.handleOnChange}> Medium</Radio>
+        <Radio name={'difficulty'} value={'hard'} checked={this.state.selected === 'hard'} onChange={this.handleOnChange}> Hard</Radio>
+        <Radio name={'difficulty'} value={'very-hard'} checked={this.state.selected === 'very-hard'} onChange={this.handleOnChange}> Very Hard</Radio>
+        <Radio name={'difficulty'} value={'insane'} checked={this.state.selected === 'insane'} onChange={this.handleOnChange}> Insane</Radio>
+        <Radio name={'difficulty'} value={'inhuman'} checked={this.state.selected === 'inhuman'} onChange={this.handleOnChange}> Inhuman</Radio>
+        <Link to='/puzzle'>
+          <ButtonFullWidth 
+            color={'dark'} 
+            text={'Start Puzzle'} 
+            handleOnClick={this.handleOnClick} 
+          />
+        </Link>
+      </Container>
     )
   }
 }
