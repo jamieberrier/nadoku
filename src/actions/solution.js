2ae65dfcc07449360578c3d0cc84705e3c051454
jamieberrier <berrier.jamie@gmail.com>
2020-10-20 13:12:36 -0700

    adds SudokuToolCollection and generateSolution

--- src/actions/solution.js
+++ src/actions/solution.js
@@ -1,13 +1,25 @@
+import SudokuToolCollection from 'sudokutoolcollection';
+
+const sudoku = SudokuToolCollection()
+
 // syncronous action creators
-export const setSolution = solutionString => {
+export const setSolution = solution => {
   return {
     type: 'SET_SOLUTION',
-    solutionString
+    solution
   }
 }
 
 export const clearSolution = () => {
   return {
     type: 'CLEAR_SOLUTION'
+  }
+}
+
+export const generateSolution = ({ puzzleString }) => {
+  return dispatch => {
+    const solutionString = sudoku.solver.solve(puzzleString)
+    const solution = sudoku.conversions.stringToObject(solutionString)
+    return dispatch(setSolution(solution))
   }
 }
