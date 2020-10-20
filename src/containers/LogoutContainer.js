469c1525d08dd0ee3eeec4f3f2f048f52524b5b0
jamieberrier <berrier.jamie@gmail.com>
2020-10-20 13:10:23 -0700

    chnages to class component, utilizes ButtonFullWidth Component, and clears difficulty, puzzleRaw, solution, puzzle, and selected number when clicked

--- src/containers/LogoutContainer.js
+++ src/containers/LogoutContainer.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Section, Container, Button } from 'react-bulma-components';
+import { Section, Container } from 'react-bulma-components';
 
+import { clearDifficulty } from '../actions/difficulty.js';
+import { clearPuzzle } from '../actions/puzzle.js';
+import { clearPuzzleRaw } from '../actions/puzzleRaw.js';
+import { clearSelectedNumber } from '../actions/selectedNumber.js';
+import { clearSolution } from '../actions/solution.js';
 import { logout } from '../actions/currentUser.js'
+import ButtonFullWidth from '../components/ButtonFullWidth.js';
 
-const Logout = ({ logout }) => {
+class Logout extends Component {
 
-  return (
-    <Section>
-      <Container id='LogoutContainer'>
-        <Link to='/'>
-          <Button 
-            color={'dark'}
-            fullwidth={true} 
-            onClick={logout}
-          >
-            Log Out
-          </Button>
-        </Link>
-      </Container>
-    </Section>
-  )
+  handleOnClick = () => {
+    this.props.clearDifficulty()
+    this.props.clearPuzzleRaw()
+    this.props.clearSolution()
+    this.props.clearPuzzle()
+    this.props.clearSelectedNumber()
+    this.props.logout()
+  }
+
+  render() {
+    return (
+      <Section>
+        <Container id='LogoutContainer'>
+          <Link to='/'>
+            <ButtonFullWidth text={'Log Out'} color={'dark'} handleOnClick={this.handleOnClick} />
+          </Link>
+        </Container>
+      </Section>
+    )
+  }
 }
 
-export default connect(null, { logout })(Logout);
+export default connect(null, { clearDifficulty,  clearPuzzle, clearPuzzleRaw, clearSelectedNumber, clearSolution, logout })(Logout);
