7e52045cdb6f8e1579024297c07483737caef74f
jamieberrier <berrier.jamie@gmail.com>
2020-10-21 14:53:38 -0700

    creates Modal to display if puzzle is solved

--- src/containers/PuzzleContainer.js
+++ src/containers/PuzzleContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Section, Container } from 'react-bulma-components';
+import { Section, Container, Modal, Heading } from 'react-bulma-components';
 
 import DifficultyContainer from './DifficultyContainer';
 import NewGameContainer from './NewGameContainer';
@@ -12,6 +12,9 @@ import { checkIfSolved } from '../actions/isSolved.js';
 import { updateCellValue } from '../actions/puzzle.js';
 
 class PuzzleContainer extends Component {
+  state = {
+    showModal: true
+  }
 
   componentDidUpdate(prevProps) {
     if (prevProps.puzzle !== this.props.puzzle) {
@@ -40,10 +43,25 @@ class PuzzleContainer extends Component {
       this.props.updateCellValue(rowindex, id, value)
     }
   }
+
+  handleOnClose = () => {
+    this.setState({
+      showModal: false
+    })
+  }
   
   render() {
     return (
       <Section id='PuzzleSection'>
+        {/* if puzzle is solved */}
+        {this.props.isSolved && 
+          <Modal show={this.state.showModal} onClose={this.handleOnClose}>
+            <Modal.Content className="notification is-success has-text-centered has-text-white">
+              <Heading>Puzzle Solved!</Heading>
+            </Modal.Content>
+          </Modal>
+        }
+        {/* if puzzle is generated */}
         {this.props.puzzle.length === 9 &&
           <Container id='PuzzleContainer'>
             <PuzzleGrid puzzle={this.props.puzzle} handleOnChange={this.handleOnChange} handleOnClick={this.handleOnClick} />
@@ -51,7 +69,7 @@ class PuzzleContainer extends Component {
             <NewGameContainer />
           </Container>
         }
-        {this.props.isSolved && alert('Solved!')}
+        {/* if no puzzle is generated */}
         {this.props.puzzle.length === 0 &&
           <DifficultyContainer />
         }
