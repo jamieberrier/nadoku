481eb726ace986eceda0a8df3698c18d5c5fe4d2
jamieberrier <berrier.jamie@gmail.com>
2020-10-22 10:34:25 -0700

    uses renamed Login and Logout componenets

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import { Route, withRouter } from 'react-router-dom';
 
 import './App.css';
 import { getCurrentUser } from './actions/currentUser.js';
+import Login from './components/Login.js';
 import NavBar from './components/NavBar.js';
 import Signup from './components/Signup.js';
 import Welcome from './components/Welcome.js';
 import DifficultyContainer from './containers/DifficultyContainer.js';
-import LoginContainer from './containers/LoginContainer.js';
-import LogoutContainer from './containers/LogoutContainer.js';
+import Logout from './components/Logout.js';
 import PuzzleContainer from './containers/PuzzleContainer';
 
 class App extends Component {
@@ -24,10 +24,10 @@ class App extends Component {
       <div className="App">
         <NavBar />
         <Route exact path='/' render={() => loggedIn ? <DifficultyContainer /> : <Welcome />}/>
-        <Route exact path='/login' component={LoginContainer}/>
+        <Route exact path='/login' component={Login}/>
         <Route exact path='/signup' component={Signup}/>
         <Route exact path='/puzzle' component={PuzzleContainer}/>
-        {loggedIn && <LogoutContainer />}
+        {loggedIn && <Logout />}
       </div>
     );
   }
