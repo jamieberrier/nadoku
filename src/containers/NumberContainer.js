dc7f35645a0d265a228f932c6e1b0146eb68c027
jamieberrier <berrier.jamie@gmail.com>
2020-10-27 11:47:06 -0700

    removes Section

--- src/containers/NumberContainer.js
+++ src/containers/NumberContainer.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Section } from 'react-bulma-components';
 
 import NumberPad from '../components/NumberPad';
 import { setSelectedNumber } from '../actions/selectedNumber.js';
@@ -31,9 +30,7 @@ class NumberContainer extends Component {
 
   render() {
     return (
-      <Section id='NumberSection'>
-        <NumberPad handleOnClick={this.handleOnClick} />
-      </Section>
+      <NumberPad handleOnClick={this.handleOnClick} />
     )
   }
 }
