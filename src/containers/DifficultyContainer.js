9dd7e5f67b390e7b3ac3de34c0ce3d7545102cdd
jamieberrier <berrier.jamie@gmail.com>
2020-10-27 14:11:42 -0700

    moves logic to OptionsContainer

--- src/containers/DifficultyContainer.js
+++ src/containers/DifficultyContainer.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Section, Container, Form, Heading } from 'react-bulma-components';
+import { Container, Form, Heading } from 'react-bulma-components';
 
 import { setDifficulty } from '../actions/difficulty';
-import { generateRawPuzzle } from '../actions/puzzleRaw.js';
-import { generatePuzzle } from '../actions/puzzle.js';
-import { generateSolution } from '../actions/solution.js';
-import ButtonFullWidth from '../components/ButtonFullWidth';
-import SoundContainer from './SoundContainer.js';
 
 const { Radio } = Form;
 
@@ -23,48 +17,28 @@ class DifficultyContainer extends Component {
     this.setState({
       selected: event.target.value,
     })
-  }
-
-  handleOnClick = () => {
-    const difficultyLevel = this.state.selected
-    this.props.setDifficulty(difficultyLevel)
-    const raw = this.props.generateRawPuzzle(difficultyLevel)
-    this.props.generateSolution(raw)
-    this.props.generatePuzzle(raw)
+    this.props.setDifficulty(event.target.value)
   }
 
   render() {
     return (
-      <>
-        <SoundContainer />
-        <Container id='DifficultyContainer'>
-          <Heading subtitle className='has-text-info'>Choose Puzzle Difficulty</Heading>
-          <Radio name={'difficulty'} value={'easy'} checked={this.state.selected === 'easy'} onChange={this.handleOnChange}> Easy</Radio>
-          <Radio name={'difficulty'} value={'medium'} checked={this.state.selected === 'medium'} onChange={this.handleOnChange}> Medium</Radio>
-          <Radio name={'difficulty'} value={'hard'} checked={this.state.selected === 'hard'} onChange={this.handleOnChange}> Hard</Radio>
-          <Radio name={'difficulty'} value={'very-hard'} checked={this.state.selected === 'very-hard'} onChange={this.handleOnChange}> Very Hard</Radio>
-          <Radio name={'difficulty'} value={'insane'} checked={this.state.selected === 'insane'} onChange={this.handleOnChange}> Insane</Radio>
-          <Radio name={'difficulty'} value={'inhuman'} checked={this.state.selected === 'inhuman'} onChange={this.handleOnChange}> Inhuman</Radio>
-        </Container>
-        <Section>
-          <Link to='/puzzle'>
-            <ButtonFullWidth 
-              color={'success'} 
-              text={'Start Puzzle'} 
-              handleOnClick={this.handleOnClick} 
-            />
-          </Link>
-        </Section>
-      </>
+      <Container id='DifficultyContainer'>
+        <Heading subtitle className='has-text-info'>Choose Puzzle Difficulty</Heading>
+        <Radio name={'difficulty'} value={'easy'} checked={this.state.selected === 'easy'} onChange={this.handleOnChange}> Easy</Radio>
+        <Radio name={'difficulty'} value={'medium'} checked={this.state.selected === 'medium'} onChange={this.handleOnChange}> Medium</Radio>
+        <Radio name={'difficulty'} value={'hard'} checked={this.state.selected === 'hard'} onChange={this.handleOnChange}> Hard</Radio>
+        <Radio name={'difficulty'} value={'very-hard'} checked={this.state.selected === 'very-hard'} onChange={this.handleOnChange}> Very Hard</Radio>
+        <Radio name={'difficulty'} value={'insane'} checked={this.state.selected === 'insane'} onChange={this.handleOnChange}> Insane</Radio>
+        <Radio name={'difficulty'} value={'inhuman'} checked={this.state.selected === 'inhuman'} onChange={this.handleOnChange}> Inhuman</Radio>
+      </Container>      
     )
   }
 }
 
-const mapStateToProps = ({ difficulty, puzzleRaw }) => {
+const mapStateToProps = ({ difficulty }) => {
   return {
-    difficulty,
-    puzzleRaw
+    difficulty
   }
 }
 
-export default connect(mapStateToProps, { setDifficulty, generateRawPuzzle, generatePuzzle, generateSolution })(DifficultyContainer);
+export default connect(mapStateToProps, { setDifficulty })(DifficultyContainer);
