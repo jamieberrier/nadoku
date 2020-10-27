68ea901029d1094f28d0399a0ea5a92310881aed
jamieberrier <berrier.jamie@gmail.com>
2020-10-27 14:31:19 -0700

    wraps container in section

--- src/containers/DifficultyContainer.js
+++ src/containers/DifficultyContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Container, Form, Heading } from 'react-bulma-components';
+import { Container, Form, Heading, Section } from 'react-bulma-components';
 
 import { setDifficulty } from '../actions/difficulty';
 
@@ -22,15 +22,17 @@ class DifficultyContainer extends Component {
 
   render() {
     return (
-      <Container id='DifficultyContainer'>
-        <Heading subtitle className='has-text-info'>Choose Puzzle Difficulty</Heading>
-        <Radio name={'difficulty'} value={'easy'} checked={this.state.selected === 'easy'} onChange={this.handleOnChange}> Easy</Radio>
-        <Radio name={'difficulty'} value={'medium'} checked={this.state.selected === 'medium'} onChange={this.handleOnChange}> Medium</Radio>
-        <Radio name={'difficulty'} value={'hard'} checked={this.state.selected === 'hard'} onChange={this.handleOnChange}> Hard</Radio>
-        <Radio name={'difficulty'} value={'very-hard'} checked={this.state.selected === 'very-hard'} onChange={this.handleOnChange}> Very Hard</Radio>
-        <Radio name={'difficulty'} value={'insane'} checked={this.state.selected === 'insane'} onChange={this.handleOnChange}> Insane</Radio>
-        <Radio name={'difficulty'} value={'inhuman'} checked={this.state.selected === 'inhuman'} onChange={this.handleOnChange}> Inhuman</Radio>
-      </Container>      
+      <Section>
+        <Container id='DifficultyContainer'>
+          <Heading subtitle className='has-text-info'>Choose Puzzle Difficulty</Heading>
+          <Radio name={'difficulty'} value={'easy'} checked={this.state.selected === 'easy'} onChange={this.handleOnChange}> Easy</Radio>
+          <Radio name={'difficulty'} value={'medium'} checked={this.state.selected === 'medium'} onChange={this.handleOnChange}> Medium</Radio>
+          <Radio name={'difficulty'} value={'hard'} checked={this.state.selected === 'hard'} onChange={this.handleOnChange}> Hard</Radio>
+          <Radio name={'difficulty'} value={'very-hard'} checked={this.state.selected === 'very-hard'} onChange={this.handleOnChange}> Very Hard</Radio>
+          <Radio name={'difficulty'} value={'insane'} checked={this.state.selected === 'insane'} onChange={this.handleOnChange}> Insane</Radio>
+          <Radio name={'difficulty'} value={'inhuman'} checked={this.state.selected === 'inhuman'} onChange={this.handleOnChange}> Inhuman</Radio>
+        </Container>      
+      </Section>
     )
   }
 }
