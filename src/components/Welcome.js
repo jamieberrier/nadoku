4259fc8a98d54d6504efe4512e04883537333802
jamieberrier <berrier.jamie@gmail.com>
2020-10-29 14:31:34 -0700

    increases heading size, adds subtitle, and changes button colors

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,14 +7,15 @@ import { Section, Container, Button, Heading } from 'react-bulma-components';
 const Welcome = () => {
   return (
     <Section id='WelcomeSection'>
-      <Heading size={5}>Welcome to Nadoku!</Heading>
+      <Heading className='has-text-info'>Welcome to Nadoku!</Heading>
+      <Heading subtitle>Please Log In or Sign Up</Heading>
       <Container id='WelcomeContainer'>
         <Button.Group position={'centered'}>
           <Link to='/login'>
-            <Button color={'success'}>Log In</Button>
+            <Button color={'info'}>Log In</Button>
           </Link>
           <Link to='/signup'>
-            <Button color={'primary'}>Sign Up</Button>
+            <Button color={'info'} outlined={true}>Sign Up</Button>
           </Link>
         </Button.Group>
       </Container>
