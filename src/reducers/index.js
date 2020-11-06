e46d0fb3dbc4646614de53ca8fd853be774d60dc
jamieberrier <berrier.jamie@gmail.com>
2020-11-06 15:43:44 -0800

    creates index to export combineReducers

---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+import { combineReducers } from 'redux';
+import currentUser from './currentUser.js';
+import difficulty from './difficulty.js';
+import displayModal from './displayModal.js';
+import isSolved from './isSolved.js';
+import loginForm from './loginForm.js';
+import puzzle from './puzzle.js';
+import selectedNumber from './selectedNumber.js';
+import signupForm from './signupForm.js';
+import solution from './solution.js';
+import sound from './sound.js';
+
+export default combineReducers({
+  currentUser,
+  difficulty,
+  displayModal,
+  isSolved,
+  loginForm,
+  puzzle,
+  selectedNumber,
+  signupForm,
+  solution,
+  sound
+})
