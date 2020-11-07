dd0eabe52ff74a920ca7ddb4628d2882f5e2c1a0
jamieberrier <berrier.jamie@gmail.com>
2020-11-07 11:10:34 -0800

    displays number pad in 1 row on tablet or larger

--- src/components/NumberPad.js
+++ src/components/NumberPad.js
@@ -7,17 +7,30 @@ const NumberPad = ({ handleOnClick }) => {
 
   return (
     <Container id='NumberPadContainer'>
-      <Button.Group position={'centered'} size={'medium'} id='NumberPad'>
+      {/* up to 786px */}
+      <Button.Group position={'centered'} size={'medium'} id='NumberPad' className='is-hidden-tablet'>
         <Button color={'success'} rounded={true} onClick={handleOnClick} value='1'>1</Button>
         <Button color={'success'} rounded={true} onClick={handleOnClick} value='2'>2</Button>
         <Button color={'success'} rounded={true} onClick={handleOnClick} value='3'>3</Button>
       </Button.Group>
-      <Button.Group position={'centered'} size={'medium'} id='NumberPad'>
+      <Button.Group position={'centered'} size={'medium'} id='NumberPad' className='is-hidden-tablet'>
         <Button color={'success'} rounded={true} onClick={handleOnClick} value='4'>4</Button>
         <Button color={'success'} rounded={true} onClick={handleOnClick} value='5'>5</Button>
         <Button color={'success'} rounded={true} onClick={handleOnClick} value='6'>6</Button>
       </Button.Group>
-      <Button.Group position={'centered'} size={'medium'}>
+      <Button.Group position={'centered'} size={'medium'} className='is-hidden-tablet'>
+        <Button color={'success'} rounded={true} onClick={handleOnClick} value='7'>7</Button>
+        <Button color={'success'} rounded={true} onClick={handleOnClick} value='8'>8</Button>
+        <Button color={'success'} rounded={true} onClick={handleOnClick} value='9'>9</Button>
+      </Button.Group>
+      {/* 769px and above */}
+      <Button.Group position={'centered'} size={'medium'} id='NumberLine' className='is-hidden-mobile'>
+        <Button color={'success'} rounded={true} onClick={handleOnClick} value='1'>1</Button>
+        <Button color={'success'} rounded={true} onClick={handleOnClick} value='2'>2</Button>
+        <Button color={'success'} rounded={true} onClick={handleOnClick} value='3'>3</Button>
+        <Button color={'success'} rounded={true} onClick={handleOnClick} value='4'>4</Button>
+        <Button color={'success'} rounded={true} onClick={handleOnClick} value='5'>5</Button>
+        <Button color={'success'} rounded={true} onClick={handleOnClick} value='6'>6</Button>
         <Button color={'success'} rounded={true} onClick={handleOnClick} value='7'>7</Button>
         <Button color={'success'} rounded={true} onClick={handleOnClick} value='8'>8</Button>
         <Button color={'success'} rounded={true} onClick={handleOnClick} value='9'>9</Button>
