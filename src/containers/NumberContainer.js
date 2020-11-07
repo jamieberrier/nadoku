a88dbe5100b9d25a60934e71c2b797e542141aef
jamieberrier <berrier.jamie@gmail.com>
2020-11-07 11:12:14 -0800

    removes duplicated cell class reset

--- src/containers/NumberContainer.js
+++ src/containers/NumberContainer.js
@@ -5,26 +5,14 @@ import 'react-bulma-components/dist/react-bulma-components.min.css';
 
 import NumberPad from '../components/NumberPad';
 import { setSelectedNumber } from '../actions/selectedNumber.js';
-import { highlightCells, setCellClass } from '../actions/puzzle.js';
+import { highlightCells } from '../actions/puzzle.js';
 
 const NumberContainer = ({ highlightCells, setSelectedNumber }) => {
   
   const handleOnClick = event => {
     const number = event.target.value
-    const rows = document.querySelector('#PuzzleGridContainer').children
     
     setSelectedNumber(number)
-    
-    for (let i = 0; i < rows.length; i++) {
-      const row = rows[i];
-      for (let j = 0; j < row.children.length; j++) {
-        const cell = row.children[j];
-        if (cell.className.includes('selected')) {
-          cell.className = setCellClass(cell.id)
-        }
-      }
-    }
-    
     highlightCells(number)
   }
 
